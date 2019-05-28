330e8c4909248bed5842700b0e6731d6dbb1c1c5
pigutty <50047826+pigutty@users.noreply.github.com>
2019-05-29 08:36:14 +0900

    message sending function

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,6 +1,6 @@
 class GroupsController < ApplicationController
-  before_action :set_group, only: [:edit, :update]
-  
+  before_action :set_groups, only: [:edit, :update]
+
   def index
   end
 
@@ -35,6 +35,6 @@ class GroupsController < ApplicationController
   end
 
   def set_groups
-    @group = Group.find(params[:ie])
+    @group = Group.find(params[:id])
   end
 end
