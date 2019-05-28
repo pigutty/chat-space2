14ae5271b307717b8cb5e9f4a44f429cd6e39bdf
pigutty <50047826+pigutty@users.noreply.github.com>
2019-05-28 20:23:59 +0900

    setting group view(edit and new)

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,2 +1,19 @@
 class GroupsController < ApplicationController
+  def index
+  end
+
+  def new
+    @group = Group.new
+    @group.users << current_user
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
 end
