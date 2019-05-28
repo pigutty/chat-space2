fceb7282cad40809bbf85eac8a32a0b0306a014e
pigutty <50047826+pigutty@users.noreply.github.com>
2019-05-29 00:43:04 +0900

    group management edit and new

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,4 +1,6 @@
 class GroupsController < ApplicationController
+  before_action :set_group, only: [:edit, :update]
+  
   def index
   end
 
@@ -8,12 +10,31 @@ class GroupsController < ApplicationController
   end
 
   def create
+    @group = Group.new(group_params)
+    if @group.save
+      redirect_to root_path, notice: 'グループを作成しました'
+    else
+      render :new
+    end
   end
 
   def edit
   end
 
   def update
+    if @group.update(group_params)
+      redirect_to group_messages_path(@group), notide: 'グループを編集しました'
+    else
+      render :edit
+    end
   end
 
+  private
+  def group_params
+    params.require(:group).permit(:name, {user_ids:[]})
+  end
+
+  def set_groups
+    @group = Group.find(params[:ie])
+  end
 end
