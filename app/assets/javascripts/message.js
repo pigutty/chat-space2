a83287705a3541e643e227ae70e2f9c6b41d7058
pigutty <50047826+pigutty@users.noreply.github.com>
2019-05-30 00:13:58 +0900

    Asynchronous communication

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,4 +1,17 @@
 $(document).on('turbolinks:load',function(){
+  function buildHTML(message){
+    var image = (message.image !== null)? `<img src="${message.image}" class="content__message__image"></img>` : "" ;
+    var html = `<div class="message">
+                  <div class="message__upper">
+                    <div class="message__upper__user">${message.user_name}</div>
+                    <div class="message__upper__date">${message.created_at}</div>
+                  </div>
+                  <div class="message__lower">${message.content}</p>
+                    ${image}
+                  </div>
+                </div>`
+      return html;
+  }
   $('#new_message').on('submit',function(e){
     e.preventDefault();
     var formData = new FormData(this);
@@ -12,7 +25,9 @@ $(document).on('turbolinks:load',function(){
       contentType: false,
     })
     .done(function(data){
-      console.log(data);
+      var html = buildHTML(data);
+      $('.messages').append(html);
+      $('#new_message')[0].reset();
     })
     .fail(function(){
       console.log("失敗");
