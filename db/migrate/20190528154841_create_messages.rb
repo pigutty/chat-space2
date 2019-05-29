ba2c0fbe20758eec731c6761a1429b63ef7d3dcf
pigutty <50047826+pigutty@users.noreply.github.com>
2019-05-29 20:44:54 +0900

    Error resolution

--- db/migrate/20190528154841_create_messages.rb
+++ db/migrate/20190528154841_create_messages.rb
@@ -4,7 +4,7 @@ class CreateMessages < ActiveRecord::Migration[5.0]
     create_table :messages do |t|
       t.string :content
       t.string :image
-      t.references :group, foregn_key: true
+      t.references :group, foreign_key: true
       t.references :user, foreign_key: true
 
       t.timestamps
