0c28f92f3b3c22f20371400ed1a2edcd687a7750
pigutty <50047826+pigutty@users.noreply.github.com>
2019-05-30 20:41:37 +0900

    Create user.js

---
+++ app/assets/javascripts/user.js
@@ -0,0 +1,50 @@
+$(document).on('turbolinks:load',function(){
+  $('#user-search-field').on('keyup',function(){
+    function appendUser(user) {
+      var html = `<div id = "chat-group-user-22">
+                    <p class="chat-group-user__name">${user.name}</p>
+                    <div class="user-search-add chat-group-user__btn chat-group-user__btn--add" data-user-id="${user.id}" data-user-name="${user.name}">追加</div>
+                  </div>`
+      $('#user-search-result').append(html);
+    }
+
+    function appendUser(user) {
+      
+    }
+
+
+    function editElement(element){
+      var result = '^'+element;
+      return result;
+    }
+    var preWord;
+    var input = $('#user-search-field').val();
+    var inputs = input.split(" ").filter(function(a) { return a;});
+    var newInputs = inputs.map(editElement);
+    var word = newInputs.join("|");
+    var reg = RegExp(word);
+    if (word != preWord){
+      $.ajax({
+        type: 'GET',
+        url: '/users',
+        data: { keyword: input},
+        dataType: 'json'
+      })
+      .done(function(users) {
+        $('#user-search-result').empty();
+        if (users.length !== 0) {
+          users.forEach(function(user){
+            appendUser(user);
+          });
+        }
+        else {
+          appendErrMsgToHTML('一致するユーザーはいません');
+        }
+      })
+      .fail(function(){
+        alert('検索に失敗しました');
+      })
+    }
+    preWord = word;
+    });
+});
