c378cee0e055402d0efd4635f4b6a636416fd9af
pigutty <50047826+pigutty@users.noreply.github.com>
2019-05-31 00:53:00 +0900

    Update user.js

--- app/assets/javascripts/user.js
+++ app/assets/javascripts/user.js
@@ -82,6 +82,7 @@ $(document).on('turbolinks:load',function(){
         $(newuser).parent().remove();
         } else {
           $(newuser).parent().remove();
+          alert('すでにこのユーザーは登録されています');
         }
       })
     }
